a532f39fc0756de96bd42e13509f0376b411c6cd
antrax2013 <antrax2013@users.noreply.github.com>
2014-05-29 14:25:17 +0200

    commit new version v0.2

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ AntMessageBox
 AntMessageBox, is a pre-define jQuery ui-dialog encapsulated in Javascript object. All AntMessageBoxes are able to contain simple and/or advanced html like images, forms, tables... and callbacks when fired events of buttons in the footer.
 <a href="http://antproduction.free.fr/AntMessageBox" target="_blank">Home page</a>
 
-Release Note: V0.13 - 05/24/2014
+Release Note: V0.2 - 05/29/2014
 =============
-- When AntMessageBox is displayed without call intDialogBox, the div insert into the DOM is not removed on destroy event. [fixed]
-- Impossible to change title with intDialogBox fonction. [fixed]
+- Click event of ok button fired on keypress event of return key
+- Add 2 vars to store parameters of callback functions of AlertDialogBox and ConfirmDialogBox
+- Now you can use directly element of DOM into every AntMessageBoxes
 
--- public/js/libs/AntMessageBox.js
+++ public/js/libs/AntMessageBox.js
@@ -1,4 +1,4 @@
-﻿/*! AntMessageBox V0.13
+﻿/*! AntMessageBox V0.2
 * Copyright (c) 2014 AntProduction
 * http://antproduction.free.fr/AntMessageBox
 * https://github.com/antrax2013/AntMessageBox
@@ -13,10 +13,11 @@
 var AntMessageBox = function () {
     this.box = null
     this.title = null
+	this.callbackOkParams = []
+	this.callbackCancelParams = []
     var _initWithTarget = false
         
-	//this.initWithTarget = function() { return _initWithTarget},
-    this.initDialogBox = function (p_title, p_elmt) {
+	this.initDialogBox = function (p_title, p_elmt) {
         this.box = (p_elmt != undefined && p_elmt.trim() != "") ? $(p_elmt) : null;
         this.title = (p_title != undefined)? p_title:"";
 
@@ -28,22 +29,24 @@ var AntMessageBox = function () {
         }
 		else _initWithTarget = true;
         
+		var box = this.box;
+		
         this.box.dialog({
             autoOpen: false,
             width:'auto',
             show: { effect: "blind" },
             hide: { effect: "blind" },
-            buttons: { Ok: function () { $(this).html = ""; $(this).dialog("close"); } },
+            buttons: { Ok: function () { $(this).html = ""; box.dialog("close"); } },
             title: this.title,
-            close: function (event, ui) { if (_initWithTarget) $(this).dialog("close"); else { $(this).dialog("destroy"); $(this).remove(); } }
+            close: function (event, ui) { if (_initWithTarget) box.dialog("close"); else { box.dialog("destroy"); box.remove(); } }
         });
-        
+         
 
     },
 
 	this.MsgDialogBox = function (p_content, p_modal) {
 	    if (_initWithTarget == false) this.initDialogBox((this.title != null && this.title.length)? this.title:"Info");
-	    this.box.html(p_content);
+	    if(p_content != null || p_content !=undefined) this.box.html(p_content);
 	    this.box.dialog({ dialogClass: "info" });
 	    this.box.dialog("option", "modal", p_modal==true);
 	    this.box.dialog("open");
@@ -51,39 +54,60 @@ var AntMessageBox = function () {
 
     this.AlertDialogBox = function (p_content, p_ok_click_callback, p_label_ok) {
         if (_initWithTarget == false) this.initDialogBox((this.title != null && this.title.length)? this.title:"Alert");
-        this.box.html(p_content);
+        if(p_content != null || p_content !=undefined) this.box.html(p_content);
         this.box.dialog("option", "modal", true);
         this.box.dialog({ dialogClass: "alert" });
+		
+		var paramsOk = ((this.callbackOkParams != [])?this.callbackOkParams:null);
+		var box = this.box;
         this.box.dialog("option", "buttons",
             [
                 {
                     text: p_label_ok != null ? p_label_ok : "Ok",
-                    click: function () { if (typeof (p_ok_click_callback) == 'function') { p_ok_click_callback(); } $(this).dialog("close"); }
+                    click: function () { if (typeof (p_ok_click_callback) == 'function') { p_ok_click_callback(paramsOk); } box.dialog("close"); }
                 }
             ]);
+		
+		this.box.keypress(function (e) {
+            if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {//Bouton entrée
+                $(this).dialog("option").buttons[0].click();
+            }
+		});
+		
         this.box.dialog("open");
     }
 
     this.ConfirmDialogBox = function (p_content, p_ok_click_callback, p_cancel_click_callback, p_label_ok, p_label_cancel) {
         if (_initWithTarget == false) this.initDialogBox((this.title != null && this.title.length)? this.title:"Confirm");
-        this.box.html(p_content);
+        if(p_content != null || p_content !=undefined) this.box.html(p_content);
         this.box.dialog("option", "modal", true);
+		
+		var paramsOk = ((this.callbackOkParams != [])?this.callbackOkParams:null);
+		var paramsCancel = ((this.callbackCancelParams!= [])?this.callbackCancelParams:null);
+		var box = this.box;
         this.box.dialog("option", "buttons",
             [
                 {
                     text: p_label_ok != null ? p_label_ok : "Ok",
-                    click: function () { if (typeof (p_ok_click_callback) == 'function') { p_ok_click_callback(); } $(this).dialog("close"); }
+                    click: function () { if (typeof (p_ok_click_callback) == 'function') { p_ok_click_callback(paramsOk); } box.dialog("close"); },
                 },
                 {
                     text: p_label_cancel != null ? p_label_cancel : "Cancel",
-                    click: function () { if (typeof (p_cancel_click_callback) == 'function') { p_cancel_click_callback.call() } $(this).dialog("close"); }
+                    click: function () { if (typeof (p_cancel_click_callback) == 'function') { p_cancel_click_callback(paramsCancel); } box.dialog("close"); }
                 }
             ]);
-        this.box.dialog("open");
+			
+		this.box.keypress(function (e) {
+            if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {//Bouton entrée
+                $(this).dialog("option").buttons[0].click();
+            }
+		});
+		this.box.dialog("open");
     }
 
     //Gestion des événements clavier sur element
     this.KeyEvent = function (idBoutonValide, idElement) {
+		console.log(idBoutonValide+" "+idElement);
         this.box.find("#" + idElement).keypress(function (e) {
             if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {//Bouton entrée
                 $("#" + idBoutonValide).click();
